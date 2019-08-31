6669109a34943ea34d64ae8594fb7da464a66f80
nontoa <nicolasnc010@gmail.com>
2019-08-30 23:45:02 -0500

    Completo hasta ejercicio 8

--- src/main/java/edu/escuelaing/CNYT/app/App.java
+++ src/main/java/edu/escuelaing/CNYT/app/App.java
@@ -69,6 +69,8 @@ public class App {
                     String[] parts = numero.split(" ");
                     vector1.add(new Complejo(Double.parseDouble(parts[0]), Double.parseDouble(parts[1])));
                 }
+                reader.close();
+                reader2.close();
                 inversaVector();
                 break;
             case (3):
@@ -83,6 +85,7 @@ public class App {
                     String[] parts = numero.split(" ");
                     vector1.add(new Complejo(Double.parseDouble(parts[0]), Double.parseDouble(parts[1])));
                 }
+                reader2.close();
                 multiplicacionEscalarVectores();
                 break;
             case (4):
@@ -106,26 +109,109 @@ public class App {
                 }
                 System.out.println("Ingrese los datos para la matriz B separado por un espacio la parte real de la imaginaria");
                 for (int f = 0; f < x; f++) {
-                    System.out.println("Ingrese los datos de la fila " + f);                    
+                    System.out.println("Ingrese los datos de la fila " + f);
                     for (int t = 0; t < y; t++) {
                         numero = reader3.nextLine();
                         String[] parts = numero.split(" ");
                         matB[f][t] = new Complejo(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
                     }
                 }
+                reader3.close();
                 adicionMatrices(matA, matB, x, y);
                 break;
             case (5):
-                inversaMatrices();
+                Scanner input = new Scanner(System.in);
+                System.out.println("Digite la dimension de la matriz: ");
+                int n = input.nextInt();
+                double a[][] = new double[n][n];
+                System.out.println("Digite los elementos de la matriz: ");
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        a[i][j] = input.nextDouble();
+                    }
+                }
+                double d[][] = inversaMatrices(a);
+
+                System.out.println("La inversa es: ");
+
+                for (int i = 0; i < n; ++i) {
+                    for (int j = 0; j < n; ++j) {
+                        System.out.print(round.format(d[i][j]) + "  ");
+                    }
+
+                    System.out.println();
+                }
+                input.close();
                 break;
             case (6):
-                multiplicacionEscalarMatrices();
+                Scanner reader4 = new Scanner(System.in);
+                int de;
+                System.out.println("Ingrese la dimension de la matriz");
+                de = reader2.nextInt();
+                Complejo[][] mat1 = new Complejo[de][de];
+                Complejo[][] mat2 = new Complejo[de][de];
+                System.out.println("Ingrese los datos para la matriz A separado por un espacio la parte real de la imaginaria");
+                for (int f = 0; f < de; f++) {
+                    System.out.println("Ingrese los datos de la fila " + f);
+                    for (int t = 0; t < de; t++) {
+                        numero = reader4.nextLine();
+                        String[] parts = numero.split(" ");
+                        mat1[f][t] = new Complejo(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
+                    }
+                }
+                System.out.println("Ingrese los datos para la matriz B separado por un espacio la parte real de la imaginaria");
+                for (int f = 0; f < de; f++) {
+                    System.out.println("Ingrese los datos de la fila " + f);
+                    for (int t = 0; t < de; t++) {
+                        numero = reader4.nextLine();
+                        String[] parts = numero.split(" ");
+                        mat2[f][t] = new Complejo(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
+                    }
+                }
+                reader4.close();
+                multiplicacionEscalarMatrices(mat1, mat2, de);
                 break;
             case (7):
-                matrizTraspuesta();
+                Scanner reader5 = new Scanner(System.in);
+                int x1,
+                 y1;
+                System.out.println("Ingrese el numero de filas");
+                x1 = reader2.nextInt();
+                System.out.println("Ingrese el numero de columas");
+                y1 = reader2.nextInt();
+                Complejo[][] matA1 = new Complejo[x1][y1];
+                System.out.println("Ingrese los datos para la matriz separado por un espacio la parte real de la imaginaria");
+                for (int f = 0; f < x1; f++) {
+                    System.out.println("Ingrese los datos de la fila " + f);
+                    for (int t = 0; t < y1; t++) {
+                        numero = reader5.nextLine();
+                        String[] parts = numero.split(" ");
+                        matA1[f][t] = new Complejo(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
+                    }
+                }
+                reader5.close();
+                matrizTraspuesta(matA1, x1, y1);
                 break;
             case (8):
-                matrizConjugada();
+                Scanner reader6 = new Scanner(System.in);
+                int x2,
+                 y2;
+                System.out.println("Ingrese el numero de filas");
+                x2 = reader2.nextInt();
+                System.out.println("Ingrese el numero de columas");
+                y2 = reader2.nextInt();
+                Complejo[][] matA2 = new Complejo[x2][y2];
+                System.out.println("Ingrese los datos para la matriz separado por un espacio la parte real de la imaginaria");
+                for (int f = 0; f < x2; f++) {
+                    System.out.println("Ingrese los datos de la fila " + f);
+                    for (int t = 0; t < y2; t++) {
+                        numero = reader6.nextLine();
+                        String[] parts = numero.split(" ");
+                        matA2[f][t] = new Complejo(Double.parseDouble(parts[0]), Double.parseDouble(parts[1]));
+                    }
+                }
+                reader6.close();
+                matrizConjugada(matA2, x2, y2);
                 break;
             case (9):
                 matrizAdjunta();
@@ -198,35 +284,196 @@ public class App {
     private static void adicionMatrices(Complejo[][] matA, Complejo[][] matB, int x, int y) {
         System.out.println("La suma de las dos matrices da como resultado");
         for (int a = 0; a < x; a++) {
-            String res="";
+            String res = "";
             for (int b = 0; b < y; b++) {
                 if (matA[a][b].getImag() + matB[a][b].getImag() > 0) {
-                    res+=round.format(matA[a][b].getReal()+matB[a][b].getReal())+"+"+ round.format(matA[a][b].getImag()+matB[a][b].getImag())+"i  ";
+                    res += round.format(matA[a][b].getReal() + matB[a][b].getReal()) + "+" + round.format(matA[a][b].getImag() + matB[a][b].getImag()) + "i  ";
                     //System.out.println(round.format(matA[a][b].getReal()+matB[a][b].getReal())+"+"+ round.format(matA[a][b].getImag()+matB[a][b].getImag())+"i");
                 } else {
-                    res+=round.format(matA[a][b].getReal()+matB[a][b].getReal())+""+ round.format(matA[a][b].getImag()+matB[a][b].getImag())+"i  ";
+                    res += round.format(matA[a][b].getReal() + matB[a][b].getReal()) + "" + round.format(matA[a][b].getImag() + matB[a][b].getImag()) + "i  ";
                     //System.out.println(round.format(matA[a][b].getReal()+matB[a][b].getReal())+""+ round.format(matA[a][b].getImag()+matB[a][b].getImag())+"i");                    
-                }                
+                }
             }
-                System.out.println(res);
+            System.out.println(res);
         }
 
     }
 
-    private static void inversaMatrices() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static double[][] inversaMatrices(double a[][]) {
+
+        int n = a.length;
+        double x[][] = new double[n][n];
+        double b[][] = new double[n][n];
+        int index[] = new int[n];
+        for (int i = 0; i < n; ++i) {
+            b[i][i] = 1;
+        }
+
+        // Transform the matrix into an upper triangle
+        gaussian(a, index);
+
+        // Update the matrix b[i][j] with the ratios stored
+        for (int i = 0; i < n - 1; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    b[index[j]][k]
+                            -= a[index[j]][i] * b[index[i]][k];
+                }
+            }
+        }
+
+        // Perform backward substitutions
+        for (int i = 0; i < n; ++i) {
+            x[n - 1][i] = b[index[n - 1]][i] / a[index[n - 1]][n - 1];
+            for (int j = n - 2; j >= 0; --j) {
+                x[j][i] = b[index[j]][i];
+                for (int k = j + 1; k < n; ++k) {
+                    x[j][i] -= a[index[j]][k] * x[k][i];
+                }
+
+                x[j][i] /= a[index[j]][j];
+            }
+        }
+        return x;
     }
 
-    private static void multiplicacionEscalarMatrices() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static void gaussian(double a[][], int index[]) {
+
+        int n = index.length;
+        double c[] = new double[n];
+
+        // Initialize the index
+        for (int i = 0; i < n; ++i) {
+            index[i] = i;
+        }
+
+        // Find the rescaling factors, one from each row
+        for (int i = 0; i < n; ++i) {
+            double c1 = 0;
+            for (int j = 0; j < n; ++j) {
+                double c0 = Math.abs(a[i][j]);
+                if (c0 > c1) {
+                    c1 = c0;
+                }
+            }
+            c[i] = c1;
+        }
+
+        // Search the pivoting element from each column
+        int k = 0;
+        for (int j = 0; j < n - 1; ++j) {
+            double pi1 = 0;
+            for (int i = j; i < n; ++i) {
+
+                double pi0 = Math.abs(a[index[i]][j]);
+                pi0 /= c[index[i]];
+                if (pi0 > pi1) {
+                    pi1 = pi0;
+                    k = i;
+                }
+            }
+
+            // Interchange rows according to the pivoting order
+            int itmp = index[j];
+            index[j] = index[k];
+            index[k] = itmp;
+            for (int i = j + 1; i < n; ++i) {
+                double pj = a[index[i]][j] / a[index[j]][j];
+
+                // Record pivoting ratios below the diagonal
+                a[index[i]][j] = pj;
+
+                // Modify other elements accordingly
+                for (int l = j + 1; l < n; ++l) {
+                    a[index[i]][l] -= pj * a[index[j]][l];
+                }
+            }
+        }
     }
 
-    private static void matrizTraspuesta() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static void multiplicacionEscalarMatrices(Complejo[][] mat1, Complejo[][] mat2, int de) {
+        Complejo[][] res = new Complejo[de][de];
+        for (int x = 0; x < de; x++) {
+            for (int y = 0; y < de; y++) {
+                Complejo suma = new Complejo(0, 0);
+                for (int z = 0; z < de; z++) {
+                    suma = sumarComplejos(suma, multiplicacion(mat1[x][z], mat2[z][y]));
+                }
+                res[x][y] = suma;
+            }
+        }
+        System.err.println("El resultado de la multiplicacion de las matrices es: ");
+        for (int a = 0; a < de; a++) {
+            String s = "";
+            for (int b = 0; b < de; b++) {
+                s += round.format(res[a][b].getReal());
+                if (res[a][b].getImag() > 0) {
+                    s += "+" + round.format(res[a][b].getImag());
+                } else {
+                    s += round.format(res[a][b].getImag());
+                }
+                s += "i ";
+            }
+            System.out.println(s);
+        }
     }
 
-    private static void matrizConjugada() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static Complejo multiplicacion(Complejo c1, Complejo c2) {
+        double x = c1.real * c2.real - c1.imag * c2.imag;
+        double y = c1.real * c2.imag + c1.imag * c2.real;
+        return (new Complejo(x, y));
+
+    }
+
+    public static Complejo sumarComplejos(Complejo c1, Complejo c2) {
+        return new Complejo(c1.getReal() + c2.getReal(), c1.getImag() + c2.getImag());
+    }
+
+    private static void matrizTraspuesta(Complejo[][] mat, int x, int y) {
+        Complejo[][] res = new Complejo[y][x];
+        for (int a = 0; a < x; a++) {
+            for (int b = 0; b < y; b++) {
+                res[b][a] = mat[a][b];
+            }
+        }
+        System.err.println("El resultado de la matriz traspuesta es: ");
+        for (int a = 0; a < y; a++) {
+            String s = "";
+            for (int b = 0; b < x; b++) {
+                s += round.format(res[a][b].getReal());
+                if (res[a][b].getImag() > 0) {
+                    s += "+" + round.format(res[a][b].getImag());
+                } else {
+                    s += round.format(res[a][b].getImag());
+                }
+                s += "i ";
+            }
+            System.out.println(s);
+        }
+    }
+
+    private static void matrizConjugada(Complejo[][] mat, int x, int y) {
+        Complejo[][] res = new Complejo[x][y];
+        for (int a = 0; a < x; a++) {
+            for (int b = 0; b < y; b++) {
+                res[a][b] = new Complejo(mat[a][b].getReal(), mat[a][b].getImag() * -1);
+            }
+        }
+        System.err.println("El resultado de la matriz conjugada es: ");
+        for (int a = 0; a < y; a++) {
+            String s = "";
+            for (int b = 0; b < x; b++) {
+                s += round.format(res[a][b].getReal());
+                if (res[a][b].getImag() > 0) {
+                    s += "+" + round.format(res[a][b].getImag());
+                } else {
+                    s += round.format(res[a][b].getImag());
+                }
+                s += "i ";
+            }
+            System.out.println(s);
+        }
+
     }
 
     private static void matrizAdjunta() {
